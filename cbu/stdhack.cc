edb836b28e7ec1feb41cc8afeb265b3f41248458
chys <admin@chys.info>
2019-11-27 00:16:22 +0800

    cbu: Add stdhack

---
+++ cbu/stdhack.cc
@@ -0,0 +1,84 @@
+/*
+ * cbu - chys's basic utilities
+ * Copyright (c) 2019, chys <admin@CHYS.INFO>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of chys <admin@CHYS.INFO> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY chys <admin@CHYS.INFO> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL chys <admin@CHYS.INFO> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#if __has_include(<bits/c++config.h>)
+# include <bits/c++config.h>
+#endif
+
+#if defined __GLIBCXX__ && defined _GLIBCXX_USE_CXX11_ABI
+# define private public
+# define protected public
+#endif
+
+#include <string>
+
+// Hacks standard containers to add more functionality
+
+namespace cbu {
+inline namespace cbu_stdhack {
+namespace {
+
+template <typename T>
+T *extend_impl(std::basic_string<T>* buf, std::size_t n) {
+#if defined __GLIBCXX__ && defined _GLIBCXX_USE_CXX11_ABI
+  buf->reserve(buf->size() + n);
+  T *w = buf->data() + buf->size();
+  buf->_M_set_length(buf->size() + n);
+  return w;
+#else
+  buf->resize(buf->size() + n);
+  return (buf->data() + buf->size() - n);
+#endif
+}
+
+} // namespace
+
+char *extend(std::string* buf, std::size_t n) {
+  return extend_impl(buf, n);
+}
+
+wchar_t *extend(std::wstring* buf, std::size_t n) {
+  return extend_impl(buf, n);
+}
+
+char16_t *extend(std::u16string* buf, std::size_t n) {
+  return extend_impl(buf, n);
+}
+
+char32_t *extend(std::u32string* buf, std::size_t n) {
+  return extend_impl(buf, n);
+}
+
+#if defined __cpp_char8_t && __cpp_char8_t >= 201811
+char8_t *extend(std::u8string* buf, std::size_t n) {
+  return extend_impl(buf, n);
+}
+#endif
+
+} // namespace cbu_stdhack
+} // namespace cbu
