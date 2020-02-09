c7137a752eaaf3284dab7444e23b95f68bc2c863
chys <admin@chys.info>
2020-02-10 01:07:12 +0800

    cbu: Add "_v" constexpr values for type traits

--- cbu/type_traits.h
+++ cbu/type_traits.h
@@ -43,6 +43,9 @@ struct and_<std::false_type, V...> : std::false_type {};
 template <typename T, typename... V>
 struct and_<T, V...> : and_<std::bool_constant<T::value>, V...>::type {};
 
+template <typename... Traits>
+inline constexpr bool and_v = and_<Traits...>::value;
+
 // Or
 template <typename...> struct or_;
 template <> struct or_<> : std::false_type {};
@@ -53,15 +56,27 @@ struct or_<std::false_type, V...> : or_<V...>::type {};
 template <typename T, typename... V>
 struct or_<T, V...> : or_<std::bool_constant<T::value>, V...>::type {};
 
+template <typename... Traits>
+inline constexpr bool or_v = or_<Traits...>::value;
+
 // Not
 template <typename T> struct not_ : std::bool_constant<!T::value> {};
+
+template <typename T>
+inline constexpr bool not_v = !T::value;
 
 // None
 template <typename... T> struct none_ : not_<or_<T...>>::type {};
 
+template <typename... Traits>
+inline constexpr bool none_v = none_<Traits...>::value;
+
 // Check whether a type is one of many types
 template <typename T, typename... C>
 struct is_one_of : or_<std::is_same<T, C>...>::type {};
+
+template <typename T, typename... C>
+inline constexpr bool is_one_of_v = is_one_of<T, C...>::value;
 
 // Extended is_same
 template <typename...> struct is_same;
@@ -69,6 +84,8 @@ template <> struct is_same<> : std::true_type {};
 template <typename U, typename... V>
 struct is_same<U, V...> : and_<std::is_same<U, V>...>::type {};
 
+template <typename... Types>
+inline constexpr bool is_same_v = is_same<Types...>::value;
 
 } // inline namespace cbu_type_traits
 } // namespace cbu
