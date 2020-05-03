d53d03ba567502f7f078d5474a7606267f4d490e
chys <admin@chys.info>
2020-05-03 22:48:37 +0800

    cbu: Added hint.h

---
+++ cbu/cbu/common/hint.h
@@ -0,0 +1,59 @@
+/*
+ * cbu - chys's basic utilities
+ * Copyright (c) 2019, 2020, chys <admin@CHYS.INFO>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of chys <admin@CHYS.INFO> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY chys <admin@CHYS.INFO> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL chys <admin@CHYS.INFO> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <assert.h>
+
+namespace cbu {
+inline namespace cbu_hint {
+
+#ifndef __has_builtin
+# define __has_builtin(x) 0
+#endif
+
+#if __has_builtin(__builtin_assume)
+# define CBU_HINT_ASSUME(expr) __builtin_assume(expr)
+#elif (defined __GNUC__ && (__GNUC__ * 100 + __GNUC_MINOR__ >= 405)) || __has_builtin(__builtin_unreachable)
+# define CBU_HINT_ASSUME(expr) do { if (!(expr)) __builtin_unreachable(); } while(0)
+#else
+# define CBU_HINT_ASSUME(expr) ((void)0)
+#endif
+
+#define CBU_HINT_ASSERT(cond) do { assert(cond); CBU_HINT_ASSUME(cond); } while (0)
+
+// Give the compiler a hint that the pointer is guaranteed to be non-null
+template <typename T> inline T* hint_nonnull(T *ptr) noexcept {
+  CBU_HINT_ASSUME(ptr != nullptr);
+  return ptr;
+}
+
+#define CBU_HINT_NONNULL ::cbu::cbu_hint::hint_nonnull
+
+} // namespace cbu_hint
+} // namespace cbu
