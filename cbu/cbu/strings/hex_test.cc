29f72595c3ba35c7a66fa12812454a2261510014
chys <admin@chys.info>
2023-03-14 13:08:22 +0800

    cbu: Add ToHex, LittleEndian64ToHex

---
+++ cbu/cbu/strings/hex_test.cc
@@ -0,0 +1,51 @@
+/*
+ * cbu - chys's basic utilities
+ * Copyright (c) 2023, chys <admin@CHYS.INFO>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of chys <admin@CHYS.INFO> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY chys <admin@CHYS.INFO> ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL chys <admin@CHYS.INFO> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "cbu/strings/hex.h"
+
+#include <string_view>
+
+#include <gtest/gtest.h>
+
+namespace cbu {
+namespace {
+
+using std::operator""sv;
+
+TEST(HexTest, ToHexTest) {
+  char buf[32];
+
+  ToHex(buf, 0xab);
+  ASSERT_EQ(std::string_view(buf, 2), "ab"sv);
+
+  LittleEndian64ToHex(buf, 0x1234567890abcdef);
+  ASSERT_EQ(std::string_view(buf, 16), "efcdab9078563412"sv);
+}
+
+}  // namespace
+}  // namespace cbu
