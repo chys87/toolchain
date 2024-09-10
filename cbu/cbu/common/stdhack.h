4ca206085849b0c1a719116f2b992b43ac1a488e
chys <admin@chys.info>
2024-09-10 17:56:02 +0800

    cbu: Re-implement stdhack
    * Remove '#define private public' hack (undefined behavior per standard)
    * Move implementation to header to allow constexpr-ness

--- cbu/cbu/common/stdhack.h
+++ cbu/cbu/common/stdhack.h
@@ -30,66 +30,138 @@
 
 #include <string>
 
+#include "cbu/common/concepts.h"
 #include "cbu/tweak/tweak.h"
 
 // Hacks standard containers to add more functionality
 
 namespace cbu {
+namespace stdhack_detail {
 
-char *extend(std::string* buf, std::size_t n) CBU_MEMORY_NOEXCEPT;
-wchar_t *extend(std::wstring* buf, std::size_t n) CBU_MEMORY_NOEXCEPT;
-char16_t *extend(std::u16string* buf, std::size_t n) CBU_MEMORY_NOEXCEPT;
-char32_t *extend(std::u32string* buf, std::size_t n) CBU_MEMORY_NOEXCEPT;
-#if defined __cpp_char8_t && __cpp_char8_t >= 201811
-char8_t *extend(std::u8string* buf, std::size_t n) CBU_MEMORY_NOEXCEPT;
+template <typename T>
+concept HasResizeDefaultInit = requires(std::basic_string<T>* s,
+                                        std::size_t sz) {
+  {s->__resize_default_init(sz)};
+};
+
+// We can test for macro __cpp_lib_string_resize_and_overwrite, but using
+// concept is more direct.
+template <typename T>
+concept HasResizeAndOverwrite =
+    requires(std::basic_string<T>* s, std::size_t sz) {
+      {
+        s->resize_and_overwrite(sz, [sz](T* w) { return 0; })
+      };
+    };
+
+#if defined __GLIBCXX__ && defined _GLIBCXX_USE_CXX11_ABI
+template <typename C, void (std::basic_string<C>::*foo)(std::size_t)>
+struct MLengthAccessor {
+  friend constexpr void _M_length(std::basic_string<C>* ptr,
+                                  std::size_t l) noexcept {
+    (ptr->*foo)(l);
+  }
+};
+
+template <typename C, void (std::basic_string<C>::*foo)(std::size_t)>
+struct MSetLengthAccessor {
+  friend constexpr void _M_set_length(std::basic_string<C>* ptr,
+                                      std::size_t l) noexcept {
+    (ptr->*foo)(l);
+  }
+};
+
+template struct MLengthAccessor<char, &std::string::_M_length>;
+template struct MLengthAccessor<wchar_t, &std::wstring::_M_length>;
+template struct MLengthAccessor<char8_t, &std::u8string::_M_length>;
+template struct MLengthAccessor<char16_t, &std::u16string::_M_length>;
+template struct MLengthAccessor<char32_t, &std::u32string::_M_length>;
+template struct MSetLengthAccessor<char, &std::string::_M_set_length>;
+template struct MSetLengthAccessor<wchar_t, &std::wstring::_M_set_length>;
+template struct MSetLengthAccessor<char8_t, &std::u8string::_M_set_length>;
+template struct MSetLengthAccessor<char16_t, &std::u16string::_M_set_length>;
+template struct MSetLengthAccessor<char32_t, &std::u32string::_M_set_length>;
+
+constexpr void _M_length(std::string*, std::size_t) noexcept;
+constexpr void _M_length(std::wstring*, std::size_t) noexcept;
+constexpr void _M_length(std::u8string*, std::size_t) noexcept;
+constexpr void _M_length(std::u16string*, std::size_t) noexcept;
+constexpr void _M_length(std::u32string*, std::size_t) noexcept;
+constexpr void _M_set_length(std::string*, std::size_t) noexcept;
+constexpr void _M_set_length(std::wstring*, std::size_t) noexcept;
+constexpr void _M_set_length(std::u8string*, std::size_t) noexcept;
+constexpr void _M_set_length(std::u16string*, std::size_t) noexcept;
+constexpr void _M_set_length(std::u32string*, std::size_t) noexcept;
 #endif
+
+}  // namespace stdhack_detail
+
+template <Std_string_char T>
+constexpr T* extend(std::basic_string<T>* buf,
+                    std::size_t n) CBU_MEMORY_NOEXCEPT {
+#if defined __GLIBCXX__ && defined _GLIBCXX_USE_CXX11_ABI
+  buf->reserve(buf->size() + n);
+  T* w = buf->data() + buf->size();
+  stdhack_detail::_M_set_length(buf, buf->size() + n);
+  return w;
+#else
+  if constexpr (stdhack_detail::HasResizeDefaultInit<T>) {
+    buf->__resize_default_init(buf->size() + n);
+  } else if constexpr (stdhack_detail::HasResizeAndOverwrite<T>) {
+    size_t target_len = buf->size() + n;
+    buf->resize_and_overwrite(target_len,
+                              [target_len](T*, std::size_t) constexpr noexcept {
+                                return target_len;
+                              });
+  } else {
+    buf->resize(buf->size() + n);
+  }
+  return (buf->data() + buf->size() - n);
+#endif
+}
 
 // Same as resize, but caller should guarantee n <= buf->size()
-void truncate_unsafe(std::string* buf, std::size_t n) noexcept;
-void truncate_unsafe(std::wstring* buf, std::size_t n) noexcept;
-void truncate_unsafe(std::u16string* buf, std::size_t n) noexcept;
-void truncate_unsafe(std::u32string* buf, std::size_t n) noexcept;
-#if defined __cpp_char8_t && __cpp_char8_t >= 201811
-void truncate_unsafe(std::u8string* buf, std::size_t n) noexcept;
+template <Std_string_char T>
+constexpr void truncate_unsafe(std::basic_string<T>* buf,
+                               std::size_t n) noexcept {
+  // assert(n <= buf->size());
+#if defined __GLIBCXX__ && defined _GLIBCXX_USE_CXX11_ABI
+  stdhack_detail::_M_set_length(buf, n);
+#else
+  // No difference, but __resize_default_init generates shorter code
+  if constexpr (stdhack_detail::HasResizeDefaultInit<T>) {
+    buf->__resize_default_init(n);
+  } else {
+    buf->resize(n);
+  }
 #endif
+}
 
 // Same as resize, but caller should guarantee n <= buf->size()
 // and (*buf)[n] == '\0'
-void truncate_unsafer(std::string* buf, std::size_t n) noexcept;
-void truncate_unsafer(std::wstring* buf, std::size_t n) noexcept;
-void truncate_unsafer(std::u16string* buf, std::size_t n) noexcept;
-void truncate_unsafer(std::u32string* buf, std::size_t n) noexcept;
-#if defined __cpp_char8_t && __cpp_char8_t >= 201811
-void truncate_unsafer(std::u8string* buf, std::size_t n) noexcept;
+template <Std_string_char T>
+constexpr void truncate_unsafer(std::basic_string<T>* buf,
+                                std::size_t n) noexcept {
+  // assert(n <= buf->size());
+  // assert((*buf)[n] == '\0');
+#if defined __GLIBCXX__ && defined _GLIBCXX_USE_CXX11_ABI
+  stdhack_detail::_M_length(buf, n);
+#else
+  // No difference, but __resize_default_init generates shorter code
+  if constexpr (stdhack_detail::HasResizeDefaultInit<T>) {
+    buf->__resize_default_init(n);
+  } else {
+    buf->resize(n);
+  }
 #endif
+}
 
 // Same as resize, but only truncates
-inline void truncate(std::string* buf, std::size_t n) noexcept {
+template <Std_string_char T>
+constexpr void truncate(std::basic_string<T>* buf, std::size_t n) noexcept {
   if (n < buf->size()) {
     truncate_unsafe(buf, n);
   }
 }
-inline void truncate(std::wstring* buf, std::size_t n) noexcept {
-  if (n < buf->size()) {
-    truncate_unsafe(buf, n);
-  }
-}
-inline void truncate(std::u16string* buf, std::size_t n) noexcept {
-  if (n < buf->size()) {
-    truncate_unsafe(buf, n);
-  }
-}
-inline void truncate(std::u32string* buf, std::size_t n) noexcept {
-  if (n < buf->size()) {
-    truncate_unsafe(buf, n);
-  }
-}
-#if defined __cpp_char8_t && __cpp_char8_t >= 201811
-inline void truncate(std::u8string* buf, std::size_t n) noexcept {
-  if (n < buf->size()) {
-    truncate_unsafe(buf, n);
-  }
-}
-#endif
 
-} // namespace cbu
+}  // namespace cbu
