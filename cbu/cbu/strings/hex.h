b21bc39e914914dfaee7ab77c81303438d6cf969
chys <admin@chys.info>
2025-07-10 11:54:55 +0800

    cbu: Fix missing include guard

--- cbu/cbu/strings/hex.h
+++ cbu/cbu/strings/hex.h
@@ -1,6 +1,6 @@
 /*
  * cbu - chys's basic utilities
- * Copyright (c) 2023-2024, chys <admin@CHYS.INFO>
+ * Copyright (c) 2023-2025, chys <admin@CHYS.INFO>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,6 +25,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#pragma once
 
 #if defined __ARM_NEON && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #  include <arm_neon.h>
