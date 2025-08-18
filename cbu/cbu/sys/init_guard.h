bc9296727f0034159811f16710be4ce203ae2e6d
chys <admin@chys.info>
2025-08-19 01:00:14 +0800

    cbu: Fix build with libc++

--- cbu/cbu/sys/init_guard.h
+++ cbu/cbu/sys/init_guard.h
@@ -1,6 +1,6 @@
 /*
  * cbu - chys's basic utilities
- * Copyright (c) 2020-2024, chys <admin@CHYS.INFO>
+ * Copyright (c) 2020-2025, chys <admin@CHYS.INFO>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,6 +31,7 @@
 #include <atomic>
 #include <cstdint>
 #include <memory>
+#include <new>
 
 #include "cbu/common/defer.h"
 
